13d6fe525e0032168012f55d603e5903914ae078
canceledperson <121642352+canceledperson@users.noreply.github.com>
2023-12-03 21:40:55 -0500

    Add "last updated" to database page

--- render.js
+++ render.js
@@ -2,8 +2,8 @@ const path = require('path');
 const fs = require('fs');
 const ejs = require('ejs');
 
-function renderFile(fileName) {
-  ejs.renderFile(path.join(fileName), {}, function(err, html) {
+function renderFile(fileName, data = {}) {
+  ejs.renderFile(path.join(fileName), data, function(err, html) {
     if (err) console.log(err);
     const outputFileName = `${fileName.split('.')[0]}.html`;
     fs.writeFileSync(path.join(outputFileName), html, 'utf8');
@@ -13,5 +13,5 @@ function renderFile(fileName) {
 renderFile("index.ejs")
 renderFile("about/index.ejs")
 renderFile("cancelations/index.ejs")
-renderFile("cancellations/index.ejs")
+renderFile("cancellations/index.ejs", {date: Date.now()})
 renderFile("submissions/index.ejs")
